43d381f136dfdb0f6dedf522fb1bdbfd39735a7e
Gabriel Arrais <arrais.gbr@usp.br>
2021-12-22 22:45:53 -0300

    Melhorando test_ia

--- Classes/test_ia.py
+++ Classes/test_ia.py
@@ -4,7 +4,7 @@ from mega_tabuleiro import MegaTabuleiro
 
 @pytest.fixture
 def ia():
-    return IA("Nome", 1)
+    return IA("Nome", 'x')
 
 @pytest.fixture
 def mega_tabuleiro():
@@ -15,8 +15,43 @@ class TestaIA():
         assert ia.receber_nome() == "Nome"
     
     def test_construtor_simbolo(self, ia):
-        assert ia.receber_simbolo() == 1
+        assert ia.receber_simbolo() == 'x'
 
     def test_preparar_jogada(self, ia, mega_tabuleiro):
+        linha, coluna, sub_linha, sub_coluna = ia.preparar_jogada(mega_tabuleiro)
+        assert mega_tabuleiro.receber_sub_posicao(linha, coluna, sub_linha, sub_coluna) == -1
+
+    def test_preparar_jogada_meio_ocupado(self, ia, mega_tabuleiro):
+        mega_tabuleiro.definir_sub_posicao(0, 0, 1, 1, 1)
+        linha, coluna, sub_linha, sub_coluna = ia.preparar_jogada(mega_tabuleiro)
+        assert mega_tabuleiro.receber_sub_posicao(linha, coluna, sub_linha, sub_coluna) == -1
+
+    def test_preparar_jogada_meio_esquerda_superior_ocupadas(self, ia, mega_tabuleiro):
+        mega_tabuleiro.definir_sub_posicao(0, 0, 1, 1, 1)
+        mega_tabuleiro.definir_sub_posicao(0, 0, 0, 0, 1)
+        linha, coluna, sub_linha, sub_coluna = ia.preparar_jogada(mega_tabuleiro)
+        assert mega_tabuleiro.receber_sub_posicao(linha, coluna, sub_linha, sub_coluna) == -1
+    
+    def test_preparar_jogada_meio_direita_superior_ocupadas(self, ia, mega_tabuleiro):
+        mega_tabuleiro.definir_sub_posicao(0, 0, 1, 1, 1)
+        mega_tabuleiro.definir_sub_posicao(0, 0, 0, 0, 1)
+        mega_tabuleiro.definir_sub_posicao(0, 0, 0, 2, 1)
+        linha, coluna, sub_linha, sub_coluna = ia.preparar_jogada(mega_tabuleiro)
+        assert mega_tabuleiro.receber_sub_posicao(linha, coluna, sub_linha, sub_coluna) == -1
+
+    def test_preparar_jogada_meio_esquerda_inferior_ocupadas(self, ia, mega_tabuleiro):
+        mega_tabuleiro.definir_sub_posicao(0, 0, 1, 1, 1)
+        mega_tabuleiro.definir_sub_posicao(0, 0, 0, 0, 1)
+        mega_tabuleiro.definir_sub_posicao(0, 0, 0, 2, 1)
+        mega_tabuleiro.definir_sub_posicao(0, 0, 2, 0, 2)
+        linha, coluna, sub_linha, sub_coluna = ia.preparar_jogada(mega_tabuleiro)
+        assert mega_tabuleiro.receber_sub_posicao(linha, coluna, sub_linha, sub_coluna) == -1
+    
+    def test_preparar_jogada_meio_direita_inferior_ocupadas(self, ia, mega_tabuleiro):
+        mega_tabuleiro.definir_sub_posicao(0, 0, 1, 1, 1)
+        mega_tabuleiro.definir_sub_posicao(0, 0, 0, 0, 1)
+        mega_tabuleiro.definir_sub_posicao(0, 0, 0, 2, 1)
+        mega_tabuleiro.definir_sub_posicao(0, 0, 2, 0, 2)
+        mega_tabuleiro.definir_sub_posicao(0, 0, 2, 2, 2)
         linha, coluna, sub_linha, sub_coluna = ia.preparar_jogada(mega_tabuleiro)
         assert mega_tabuleiro.receber_sub_posicao(linha, coluna, sub_linha, sub_coluna) == -1
